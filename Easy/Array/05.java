8430cc5acccdf8c8c1124caabf055a9ee0f2e604
chaoyoushen <yo1303512080@qq.com>
2018-08-13 10:50:47 +0800

    test

--- Easy/Array/05.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-        int num = 0;
-        for(int i = 0; i < nums.length; i++) {
-            num ^= nums[i];
-        }
-        return num;
-    }
-}
